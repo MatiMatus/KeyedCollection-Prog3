676383b60aa60b498080f60466357e0ed343bfd1
MatiMatus <62270312+MatiMatus@users.noreply.github.com>
2023-04-17 22:17:36 -0300

    Add files via upload

---
+++ KeyedCollection/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyedCollection
+{
+    public class OrderItem
+    {
+        public readonly int PartNumber;
+        public readonly string Description;        
+
+        public OrderItem(int partNumber, string description)
+        {
+            this.PartNumber = partNumber;
+            this.Description = description;            
+        }        
+    }
+}
---
+++ KeyedCollection/SimpleOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyedCollection
+{
+    public class SimpleOrder : KeyedCollection<int, OrderItem >
+    {
+        protected override int GetKeyForItem(OrderItem item)
+        {
+            return item.PartNumber;
+        }
+        internal void ChangeKey(OrderItem item, int newKey)
+        {
+            base.ChangeItemKey(item, newKey);
+        }
+    }
+}
